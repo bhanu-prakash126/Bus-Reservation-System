f9dbc3295c5fd72c977c67da6256a68c30f60c09
bhanu-prakash126 <y.bhanuprakash126@gmail.com>
2026-04-20 10:39:21 +0530

    v-4

--- src/main/java/com/demo/BusReservationSystemApplication.java
+++ src/main/java/com/demo/BusReservationSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BusResrvationSystemApplication {
+public class BusReservationSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BusResrvationSystemApplication.class, args);
+		SpringApplication.run(BusReservationSystemApplication.class, args);
 	}
 
 }
--- src/test/java/com/demo/BusReservationSystemApplicationTests.java
+++ src/test/java/com/demo/BusReservationSystemApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BusResrvationSystemApplicationTests {
+class BusReservationSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
